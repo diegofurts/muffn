a5976ffaf4dfd9e6e844b4416af77177603bf1bb
MUFFN <65376722+muffn2020@users.noreply.github.com>
2020-05-22 12:47:55 -0300

    allowing multiple inputs

--- src/datagenerator.py
+++ src/datagenerator.py
@@ -52,9 +52,74 @@ class DataGenerator(Sequence):
         for i, ID in enumerate(list_IDs_temp):
             # Store sample
             this_feat = np.load(self.gen_dir + str(ID) + '.npy')
-            X[i,] = this_feat.reshape((self.dim[0], 
-                                       self.dim[1], 
+            X[i,] = this_feat.reshape((*self.dim, 
                                        self.n_channels))
+
+            # Store class
+            y[i] = self.labels[ID]
+
+        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+
+
+class MultiDataGenerator(Sequence):
+
+    'Generates data for Keras'
+    def __init__(self, list_IDs, labels, batch_size, n_channels,
+                 n_classes, gen_dir, feat_list, shuffle=True):
+        'Initialization'
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+        self.gen_dir = gen_dir
+        self.feat_list = feat_list
+        self.dim_list = []
+        for f in feat_list:
+            example_data = np.load(gen_dir + f + "/0.npy")
+            self.dim_list.append(example_data.shape)
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        y = np.empty((self.batch_size), dtype=int)
+        X = []
+        for i in range (len(self.dim_list)):
+            X_temp = np.empty((batch_size, *dim_list[i], n_channels))
+            X.append(X_temp)
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            # Store sample
+            for j in range (len(self.dim_list)):
+                this_feat = np.load(self.gen_dir + feat_list[j] + "/" + str(ID) + '.npy')
+                X[j][i] = this_feat.reshape((*self.dim[j], 
+                                           self.n_channels))
 
             # Store class
             y[i] = self.labels[ID]
